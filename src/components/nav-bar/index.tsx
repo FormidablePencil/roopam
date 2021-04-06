863488b7ff35130e4e44daf09cc96995e9cf5c6c
Dennis Bogdan Aleksandrov <extraordinarycoutesy@gmail.com>
2021-04-06 00:15:34 -0700

    added navbar data

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -3,6 +3,7 @@ import { IoIosArrowUp } from "react-icons/io";
 import { useHistory, useLocation } from "react-router-dom";
 import "./index.scoped.sass";
 import NavbarToggleBtn from "./navbar-toggle-btn";
+import { logoImg } from "../../static-data/images";
 
 function NavBar() {
   const { pathname } = useLocation();
@@ -24,15 +25,75 @@ function NavBar() {
   const toggleProjectsDropdown = () => setOpenProjects((prev) => !prev);
   const closeNav = () => setNavbarOpen((prev) => !prev);
 
+  const navigationItems = [
+    {
+      title: "Company",
+      navigateTo: "/company",
+      dropdown: [
+        { title: "About", navigateTo: "/about" },
+        { title: "Team", navigateTo: "/team" },
+        { title: "Partners", navigateTo: "/partners" },
+        { title: "Clients", navigateTo: "/clients" },
+        { title: "Customer Testimonals", navigateTo: "/testimonals" },
+        { title: "Contact", navigateTo: "/contact" },
+      ],
+    },
+    { title: "COVID-19", navigateTo: "/covid" },
+    {
+      title: "Digital Pathology",
+      navigateTo: "/",
+      dropdown: [
+        { title: "Pathologists", navigateTo: "/pathologists" },
+        { title: "Leadership", navigateTo: "/leadership" },
+        { title: "IT", navigateTo: "/it" },
+        { title: "Patients", navigateTo: "/patients" },
+        { title: "Implementation", navigateTo: "/implementation" },
+        { title: "Integration", navigateTo: "/integration" },
+        { title: "Telepathology", navigateTo: "/telepathology" },
+        { title: "Workflow", navigateTo: "/workflow" },
+        { title: "Tumor Boards", navigateTo: "/tumor-boards" },
+        {
+          title: "Validation and Review",
+          navigateTo: "/validation-and-review",
+        },
+        { title: "Clinical Trials", navigateTo: "/clinical-trials" },
+        { title: "Support", navigateTo: "/support" },
+        { title: "SAAS Hosted Solution", navigateTo: "/saas-hosted-solution" },
+      ],
+    },
+    { title: "Solutions For", navigateTo: "/solutions" },
+    {
+      title: "Professional Services",
+      navigateTo: "/services",
+      dropdown: [
+        { title: "Physician Portal", navigateTo: "/physician-portal" },
+        {
+          title: "Ascent - Cloud-based LIS",
+          navigateTo: "/ascent-cloud-based-lis",
+        },
+        { title: "IT Services", navigateTo: "/copy-of-services-2" },
+        { title: "News & Events", navigateTo: "/news-events" },
+        {
+          title: "Awards and Recognition",
+          navigateTo: "/awards-and-recognition",
+        },
+        { title: "Blog", navigateTo: "/blog" },
+        { title: "Careers", navigateTo: "/careers" },
+      ],
+    },
+  ];
+
   return (
     <>
       <div className="absolute-container">
         <div className="container">
+          <div className="logo-container">
+            <img src={logoImg} alt="" />
+          </div>
           <NavbarToggleBtn
             navbarOpen={navbarOpen}
             setNavbarOpen={setNavbarOpen}
           />
-          <h1>Jordan Sheets</h1>
           <ul className={navbarOpen ? "active" : ""}>
             <li
               onClick={() => navigateTo("/")}
@@ -40,6 +101,7 @@ function NavBar() {
             >
               Home
             </li>
+
             <li
               className={`nav-item ${isProject} hide-on-small`}
               onClick={() => navigateTo("/projects/parallax")}
---
+++ src/static-data/navigation.ts
@@ -0,0 +1,58 @@
+  export const navigationItems = [
+    {
+      title: "Company",
+      navigateTo: "/company",
+      dropdown: [
+        { title: "About", navigateTo: "/about" },
+        { title: "Team", navigateTo: "/team" },
+        { title: "Partners", navigateTo: "/partners" },
+        { title: "Clients", navigateTo: "/clients" },
+        { title: "Customer Testimonals", navigateTo: "/testimonals" },
+        { title: "Contact", navigateTo: "/contact" },
+      ],
+    },
+    { title: "COVID-19", navigateTo: "/covid" },
+    {
+      title: "Digital Pathology",
+      navigateTo: "/",
+      dropdown: [
+        { title: "Pathologists", navigateTo: "/pathologists" },
+        { title: "Leadership", navigateTo: "/leadership" },
+        { title: "IT", navigateTo: "/it" },
+        { title: "Patients", navigateTo: "/patients" },
+        { title: "Implementation", navigateTo: "/implementation" },
+        { title: "Integration", navigateTo: "/integration" },
+        { title: "Telepathology", navigateTo: "/telepathology" },
+        { title: "Workflow", navigateTo: "/workflow" },
+        { title: "Tumor Boards", navigateTo: "/tumor-boards" },
+        {
+          title: "Validation and Review",
+          navigateTo: "/validation-and-review",
+        },
+        { title: "Clinical Trials", navigateTo: "/clinical-trials" },
+        { title: "Support", navigateTo: "/support" },
+        { title: "SAAS Hosted Solution", navigateTo: "/saas-hosted-solution" },
+      ],
+    },
+    { title: "Solutions For", navigateTo: "/solutions" },
+    {
+      title: "Professional Services",
+      navigateTo: "/services",
+      dropdown: [
+        { title: "Physician Portal", navigateTo: "/physician-portal" },
+        {
+          title: "Ascent - Cloud-based LIS",
+          navigateTo: "/ascent-cloud-based-lis",
+        },
+        { title: "IT Services", navigateTo: "/copy-of-services-2" },
+        { title: "News & Events", navigateTo: "/news-events" },
+        {
+          title: "Awards and Recognition",
+          navigateTo: "/awards-and-recognition",
+        },
+        { title: "Blog", navigateTo: "/blog" },
+        { title: "Careers", navigateTo: "/careers" },
+      ],
+    },
+  ];
+
